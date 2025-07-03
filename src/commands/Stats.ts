f15173e909d1b229dd2297fa6cfda03f53cf91e5
coledotjs <216449108+coledotjs@users.noreply.github.com>
2025-07-03 19:52:43 +0200

    fix(commands): Typo in `process`.

--- src/commands/Stats.ts
+++ src/commands/Stats.ts
@@ -49,7 +49,7 @@ export default class Stats extends Command {
 					inline: true
 				},
 				{
-					name: "Uptime (Client / Proces)",
+					name: "Uptime (Client / Process)",
 					value: `${clientUptime} / ${processUptime}`,
 					inline: true
 				},
